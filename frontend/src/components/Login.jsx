8d979df95c78d0df077c8df436b9923a271ed56b
Mogalapu Sriram Chowdary <mogalapusriram391@gmail.com>
2024-12-24 21:33:30 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
     try {
       // Directly make the API call here
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://ekyc-8o9a.onrender.com/api/auth/login",
         {
           username,
           password,
